092ad183c028948dd1612d7194927ac746209c11
Marko1808 <markovuckovic1808@gmail.com>
2018-07-10 12:16:01 +0200

    ispravka kod kopiranja cenovnika

--- Fakturisanje/src/main/java/poslovna/controller/CenovnikController.java
+++ Fakturisanje/src/main/java/poslovna/controller/CenovnikController.java
@@ -160,7 +160,7 @@ public class CenovnikController {
 			stavka.setCenovnik(saved);
 			stavka.setPopust(cenovnikStavke.getStavkeUCenovniku().get(i).getPopust());
 			stavka.setArtikal(cenovnikStavke.getStavkeUCenovniku().get(i).getArtikal());
-			stavka.setCena(cenovnikStavke.getStavkeUCenovniku().get(i).getCena() * (100 - procenat) /100);
+			stavka.setCena(cenovnikStavke.getStavkeUCenovniku().get(i).getCena() *  procenat/100);
 			stavkaService.save(stavka);
 			saved.getStavkeUCenovniku().add(stavka);
 		
