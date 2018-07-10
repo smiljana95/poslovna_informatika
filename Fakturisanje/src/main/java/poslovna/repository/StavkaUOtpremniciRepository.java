6c3ee974658c9f5a0f7ed5b7bd1d62a102984364
tttamara23 <tamara.perlinac@yahoo.com>
2018-07-10 18:46:10 +0200

    Rad sa otpremnicama : brisanje,eskport, generisanje.

---
+++ Fakturisanje/src/main/java/poslovna/repository/StavkaUOtpremniciRepository.java
@@ -0,0 +1,9 @@
+package poslovna.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import poslovna.model.StavkaUOtpremnici;
+
+public interface StavkaUOtpremniciRepository extends JpaRepository<StavkaUOtpremnici, Long>{
+
+}
